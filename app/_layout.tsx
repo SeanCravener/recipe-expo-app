246ea3a04dc189e1ed64f2c12b458230155766fa
Sean Cravener <scravenerpat@gmail.com>
2025-04-29 12:40:26 -0400

    Set up NativeWind to use for styling app and comopnents.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import "../global.css";
 import { Stack } from "expo-router";
 import { AuthProvider } from "../contexts/auth/AuthContext";
 import { QueryProvider } from "../contexts/query/QueryProvider";
