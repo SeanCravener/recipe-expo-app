e84e023334ca5f0f51b9a253528d344e3ad0dcda
Sean Cravener <scravenerpat@gmail.com>
2025-05-05 13:44:22 -0400

    Setting up application to use the NativeWind React Native Reusables library for building the custom components. Once I'm more experienced I want to build my own components from scratch.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,38 +3,92 @@ import { Stack } from "expo-router";
 import { AuthProvider } from "../contexts/auth/AuthContext";
 import { QueryProvider } from "../contexts/query/QueryProvider";
 import { FavoritesProvider } from "@/contexts/FavoritesContext";
+import {
+  Theme,
+  ThemeProvider,
+  DefaultTheme,
+  DarkTheme,
+} from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import * as React from "react";
+import { Platform } from "react-native";
+import { NAV_THEME } from "~/lib/constants";
+import { useColorScheme } from "~/lib/useColorScheme";
+
+const LIGHT_THEME: Theme = {
+  ...DefaultTheme,
+  colors: NAV_THEME.light,
+};
+const DARK_THEME: Theme = {
+  ...DarkTheme,
+  colors: NAV_THEME.dark,
+};
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from "expo-router";
 
 export default function RootLayout() {
+  const hasMounted = React.useRef(false);
+  const { colorScheme, isDarkColorScheme } = useColorScheme();
+  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
+
+  useIsomorphicLayoutEffect(() => {
+    if (hasMounted.current) {
+      return;
+    }
+
+    if (Platform.OS === "web") {
+      // Adds the background color to the html element to prevent white background on overscroll.
+      document.documentElement.classList.add("bg-background");
+    }
+    setIsColorSchemeLoaded(true);
+    hasMounted.current = true;
+  }, []);
+
+  if (!isColorSchemeLoaded) {
+    return null;
+  }
+
   return (
-    <QueryProvider>
-      <AuthProvider>
-        <FavoritesProvider>
-          <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen
-              name="(auth)/sign-in"
-              options={{
-                title: "Sign In",
-                presentation: "modal",
-              }}
-            />
-            <Stack.Screen
-              name="(auth)/sign-up"
-              options={{
-                title: "Sign Up",
-                presentation: "modal",
-              }}
-            />
-            <Stack.Screen
-              name="settings"
-              options={{
-                title: "Settings",
-                presentation: "modal",
-              }}
-            />
-          </Stack>
-        </FavoritesProvider>
-      </AuthProvider>
-    </QueryProvider>
+    <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+      <StatusBar style={isDarkColorScheme ? "light" : "dark"} />
+      <QueryProvider>
+        <AuthProvider>
+          <FavoritesProvider>
+            <Stack>
+              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              <Stack.Screen
+                name="(auth)/sign-in"
+                options={{
+                  title: "Sign In",
+                  presentation: "modal",
+                }}
+              />
+              <Stack.Screen
+                name="(auth)/sign-up"
+                options={{
+                  title: "Sign Up",
+                  presentation: "modal",
+                }}
+              />
+              <Stack.Screen
+                name="settings"
+                options={{
+                  title: "Settings",
+                  presentation: "modal",
+                }}
+              />
+            </Stack>
+          </FavoritesProvider>
+        </AuthProvider>
+      </QueryProvider>
+    </ThemeProvider>
   );
 }
+
+const useIsomorphicLayoutEffect =
+  Platform.OS === "web" && typeof window === "undefined"
+    ? React.useEffect
+    : React.useLayoutEffect;
